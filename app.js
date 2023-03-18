17bbfe898efe42b18062f62347469a3a162d3f3d
@NickLi0304 <twinpeterlee@gmail.com>
2023-03-18 10:56:55 +0800

    initialize Project

---
+++ app.js
@@ -0,0 +1,33 @@
+// Include express from node_modules
+const express = require("express");
+const app = express();
+
+// Define server related variables
+const port = 3000;
+
+// require handlebars in the project
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// setting static files
+app.use(express.static("public"));
+
+// routes setting
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  console.log(req.params.restaurant_id);
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`);
+});
